a348d426fbbf954236cbdf21773c89c0252d0601
wighawag <wighawag@gmail.com>
2025-04-11 14:37:27 +0100

    single-file test

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		// inline all imported assets
+		assetsInlineLimit: Infinity
+	}
 });
